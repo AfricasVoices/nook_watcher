93153ee102fb6e304ef65bf4b3659c54cb2fbc14
Kenneth Thuku <thuku.ken@live.com>
2020-03-18 12:45:44 +0300

    Add function to fetch cpu usage time metrics

--- functions/index.js
+++ functions/index.js
@@ -25,7 +25,21 @@ exports.fetchCPUUtilizationMetrics = functions.https.onRequest(
 
 exports.fetchCPUUsageTimeMetrics = functions.https.onRequest(
   async (request, response) => {
-    
+    const dataRequest = {
+      name: client.projectPath(request.query.projectId),
+      filter: 'metric.type="compute.googleapis.com/instance/cpu/usage_time"',
+      interval: {
+        startTime: {
+          // Limit results to the last 10 minutes
+          seconds: Number(request.query.startTime) || (Date.now() / 1000 - 60 * 10)
+        },
+        endTime: {
+          seconds: Number(request.query.endTime) || (Date.now() / 1000)
+        }
+      }
+    };
+    const [timeSeries] = await client.listTimeSeries(dataRequest);
+    response.json(timeSeries)
   }
 );
 
