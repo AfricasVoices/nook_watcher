fb9ea4e19165da534e31c463b63767715d13fcb1
Kenneth Thuku <thuku.ken@live.com>
2020-03-19 17:06:27 +0300

    Setup scheduled firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,56 +1,87 @@
+const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const monitoring = require("@google-cloud/monitoring");
 
+admin.initializeApp();
+let db = admin.firestore();
 const client = new monitoring.MetricServiceClient();
 
-exports.fetchCPUUtilizationMetrics = functions.https.onRequest(
-  async (request, response) => {
-    const dataRequest = {
-      name: client.projectPath(request.query.projectId),
-      filter: 'metric.type="compute.googleapis.com/instance/cpu/utilization"',
-      interval: {
-        startTime: {
-          // Limit results to the last 10 minutes
-          seconds: Number(request.query.startTime) || (Date.now() / 1000 - 60 * 10)
-        },
-        endTime: {
-          seconds: Number(request.query.endTime) || (Date.now() / 1000)
-        }
+exports.fetchAllMetrics = functions.pubsub
+  .schedule("every 10 minutes")
+  .onRun(context => {
+    let projectId = "";
+    fetchCPUUtilizationMetrics(projectId);
+    fetchCPUUsageTimeMetrics(projectId);
+    return null;
+  });
+
+async function fetchCPUUtilizationMetrics(projectId) {
+  const dataRequest = {
+    name: client.projectPath(projectId),
+    filter: 'metric.type="compute.googleapis.com/instance/cpu/utilization"',
+    interval: {
+      startTime: {
+        // Limit results to the last 10 minutes
+        seconds: Date.now() / 1000 - 60 * 10
+      },
+      endTime: {
+        seconds: Date.now() / 1000
       }
-    };
-    const [timeSeries] = await client.listTimeSeries(dataRequest);
-    response.json(timeSeries)
-  }
-);
+    }
+  };
+  const [timeSeries] = await client.listTimeSeries(dataRequest);
 
-exports.fetchCPUUsageTimeMetrics = functions.https.onRequest(
-  async (request, response) => {
-    const dataRequest = {
-      name: client.projectPath(request.query.projectId),
-      filter: 'metric.type="compute.googleapis.com/instance/cpu/usage_time"',
-      interval: {
-        startTime: {
-          // Limit results to the last 10 minutes
-          seconds: Number(request.query.startTime) || (Date.now() / 1000 - 60 * 10)
-        },
-        endTime: {
-          seconds: Number(request.query.endTime) || (Date.now() / 1000)
-        }
+  let metrics = getFormattedMetrics(timeSeries);
+
+  for (let metric of metrics) {
+    try {
+      await db.collection("cpu_utilization_metric").add(metric);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+async function fetchCPUUsageTimeMetrics(projectId) {
+  const dataRequest = {
+    name: client.projectPath(projectId),
+    filter: 'metric.type="compute.googleapis.com/instance/cpu/usage_time"',
+    interval: {
+      startTime: {
+        // Limit results to the last 10 minutes
+        seconds: Date.now() / 1000 - 60 * 10
+      },
+      endTime: {
+        seconds: Date.now() / 1000
       }
-    };
-    const [timeSeries] = await client.listTimeSeries(dataRequest);
-    response.json(timeSeries)
-  }
-);
+    }
+  };
+  const [timeSeries] = await client.listTimeSeries(dataRequest);
 
-exports.fetchRamUsageMetrics = functions.https.onRequest(
-  async (request, response) => {
-    
-  }
-);
+  let metrics = getFormattedMetrics(timeSeries);
 
-exports.fetchInstanceUptimeMetrics = functions.https.onRequest(
-  async (request, response) => {
-    
+  for (let metric of metrics) {
+    try {
+      await db.collection("cpu_usage_time_metric").add(metric);
+    } catch (error) {
+      console.error(error);
+    }
   }
-);
+}
+
+function getFormattedMetrics(timeSeries) {
+  return timeSeries
+    .map(series => {
+      let instanceName = series.metric.labels["instance_name"];
+      return series.points.map(point => {
+        return {
+          instanceName: instanceName,
+          datetime: admin.firestore.Timestamp.fromMillis(
+            point.interval.endTime.seconds * 1000
+          ).toDate(),
+          value: point.value.doubleValue
+        };
+      });
+    })
+    .flat();
+}
